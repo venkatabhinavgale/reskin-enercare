f4ae3117e287542e222cac1bfbf5ee962671c469
Andy Schaff <andy@portent.com>
2021-08-05 14:36:44 -0500

    adding in initial faq acf block with fields and output template

--- wp-content/themes/portent_enercare_2021/inc/acf-blocks.php
+++ wp-content/themes/portent_enercare_2021/inc/acf-blocks.php
@@ -2,58 +2,77 @@
 add_action('acf/init', 'enercare_register_theme_blocks');
 
 function enercare_register_theme_blocks() {
-// Register a general card block.
-	acf_register_block_type( array(
-		'name'            => 'card',
-		'title'           => __( 'Card' ),
-		'description'     => __( 'A standard card frame with a customizable inner panel' ),
-		'render_template' => 'partials/blocks/card/card.php',
-		'category'        => 'layout',
-		'mode'            => 'preview',
-		'supports'        => array(
-			'align' => true,
-			'mode'  => false,
-			'jsx'   => true,
-			'color' => array(
-				'background' => true,
-				'text' => false
-			)
-		),
-	) );
+  if ( function_exists( 'acf_register_block_type' ) ) {
+    $theme_data = wp_get_theme();
+		$theme_version = $theme_data->get('Version');
+  
+    // Register a general card block.
+    acf_register_block_type( array(
+      'name'            => 'card',
+      'title'           => __( 'Card' ),
+      'description'     => __( 'A standard card frame with a customizable inner panel' ),
+      'render_template' => 'partials/blocks/card/card.php',
+      'category'        => 'layout',
+      'mode'            => 'preview',
+      'supports'        => array(
+        'align' => true,
+        'mode'  => false,
+        'jsx'   => true,
+        'color' => array(
+          'background' => true,
+          'text' => false
+        )
+      ),
+    ));
 
-	// Register a email newsletter form block.
-	acf_register_block_type( array(
-		'name'            => 'email-signup',
-		'title'           => __( 'Email Newsletter' ),
-		'description'     => __( 'Form that allows a user to signup for an email newsletter' ),
-		'render_template' => 'partials/blocks/newsletter-signup/block-newsletter-signup.php',
-		'category'        => 'layout',
-		'mode'            => 'preview',
-		'supports'        => array(
-			'mode'  => false,
-			'color' => array(
-				'background' => true,
-				'text' => false,
-				'gradients' => false
-			)
-		),
-	) );
+    // Register a email newsletter form block.
+    acf_register_block_type( array(
+      'name'            => 'email-signup',
+      'title'           => __( 'Email Newsletter' ),
+      'description'     => __( 'Form that allows a user to signup for an email newsletter' ),
+      'render_template' => 'partials/blocks/newsletter-signup/block-newsletter-signup.php',
+      'category'        => 'layout',
+      'mode'            => 'preview',
+      'supports'        => array(
+        'mode'  => false,
+        'color' => array(
+          'background' => true,
+          'text' => false,
+          'gradients' => false
+        )
+      ),
+    ));
 
-	acf_register_block_type( array(
-		'name'            => 'tabbed-content-area',
-		'title'           => __( 'Tabbed Content Area' ),
-		'description'     => __( 'Form that allows a user to signup for an email newsletter' ),
-		'render_template' => 'partials/blocks/newsletter-signup/block-newsletter-signup.php',
-		'category'        => 'layout',
-		'mode'            => 'preview',
-		'supports'        => array(
-			'mode'  => false,
-			'color' => array(
-				'background' => true,
-				'text' => false,
-				'gradients' => false
-			)
-		),
-	) );
+    acf_register_block_type( array(
+      'name'            => 'tabbed-content-area',
+      'title'           => __( 'Tabbed Content Area' ),
+      'description'     => __( 'Form that allows a user to signup for an email newsletter' ),
+      'render_template' => 'partials/blocks/newsletter-signup/block-newsletter-signup.php',
+      'category'        => 'layout',
+      'mode'            => 'preview',
+      'supports'        => array(
+        'mode'  => false,
+        'color' => array(
+          'background' => true,
+          'text' => false,
+          'gradients' => false
+        )
+      ),
+    ));
+    
+    acf_register_block_type(array(
+			'name' => 'enercare-faqs',
+			'title' => __('FAQs', 'enercare'),
+			'render_template' => 'partials/blocks/block-faqs.php',
+			'category' => 'formatting',
+			'icon' => 'testimonial',
+			'mode' => 'auto',
+			'keywords' => array('faq', 'frequently', 'question', 'answer', 'enercare', 'portent'),
+			/*'enqueue_assets' => function() {
+				wp_enqueue_style( 'block-featured-content-style', get_template_directory_uri() . '/assets/css/block-faqs.css', array('ea-style'), false, 'screen');
+			},*/
+		));
+  
+  }
 
 }
---
+++ wp-content/themes/portent_enercare_2021/partials/blocks/block-faqs.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Card Block
+ *
+ * @package      Enercare
+ * @author       Portent
+ * @since        1.0.0
+ * @license      GPL-2.0+
+ **/
+
+$heading = get_field( 'header' );
+$content = get_field( 'content' );
+$faqs = get_field( 'faqs' );
+
+echo '<div class="block-faqs block-faqs--tablet--'.$tablet_appearance.'">';
+
+if( !empty( $heading ) )
+	echo '<h3 class="block-faqs--header">' . esc_html( $heading ) . '</h3>';
+if( !empty( $content ) )
+	echo '<p class="block-faqs--content">' . esc_html( $content ) . '</p>';
+
+if( !empty( $faqs ) )  {
+  foreach ($faqs as $faq) {
+    echo '<div class="block-faqs--faq-container">';
+    echo esc_html($faq->post_title);
+    
+    echo '<div class="block-faqs--faq-answer-container">';
+    echo esc_html($faq->post_content);
+    echo '</div>';
+    
+    echo '</div>';
+  }
+  echo '<div class="block-faqs--cta"><a href="' . get_bloginfo('url') . '/faqs" class="btn">See All FAQs</a></div>';
+}
+
+echo '</div>';
