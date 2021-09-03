3a638937fc10cfbd180f36c7ccb093614a152e3d
Andy Schaff <andy@portent.com>
2021-09-03 14:00:37 -0500

    fixing header for when there is no location selected (aggregate of all)

--- wp-content/plugins/enercare-google-reviews/enercare-google-reviews.php
+++ wp-content/plugins/enercare-google-reviews/enercare-google-reviews.php
@@ -596,13 +596,13 @@ function ecreviews_location_block_render_callback( $attributes, $content ) {
       $output .= '<!-- enercare reviews for ' . $location_name . ' -->' . "\n";  
     }
     //$output .= '<h2>Reviews for ' . $location_name . '</h2>' . "\n";
-    $output .= '<section class="block-reviews alignwide">';
-        //@todo need to find a way to pull this out of this template and make the image and text editable
-        $output .= '<div class="block-reviews__section-heading">';
-            $output .= '<img width="160px" height="160px" class="block-reviews__stamp" alt="" role="presentation" src="' . plugin_dir_url(__FILE__) . 'img/canadian-owned-operated.svg">';
-            $output .= '<div class="block-reviews__section-heading__title">';
-                $output .= '<h2 class="block-reviews__section-heading__header">Customer Reviews</h2>' . "\n";
   }
+  $output .= '<section class="block-reviews alignwide">';
+  //@todo need to find a way to pull this out of this template and make the image and text editable
+  $output .= '<div class="block-reviews__section-heading">';
+      $output .= '<img width="160px" height="160px" class="block-reviews__stamp" alt="" role="presentation" src="' . plugin_dir_url(__FILE__) . 'img/canadian-owned-operated.svg">';
+      $output .= '<div class="block-reviews__section-heading__title">';
+          $output .= '<h2 class="block-reviews__section-heading__header">Customer Reviews</h2>' . "\n";
 
   $total_reviews = ECReviews::getReviewsCount($location_id);
   $aggregate_rating = ECReviews::getAggregateRating($location_id);
