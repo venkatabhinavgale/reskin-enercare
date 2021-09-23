6c359d72252bb3bc9e7bc2036a73cafee32364d4
Jeremiah Bratton <jeremiahbratton@me.com>
2021-09-23 12:42:58 -0700

    Finishing up some tabbed content styles

--- wp-content/themes/portent_enercare_2021/assets/js/block--carousel.js
+++ wp-content/themes/portent_enercare_2021/assets/js/block--carousel.js
@@ -1,54 +1,58 @@
 "use strict";
 
 window.addEventListener('load', function () {
-  var carousel = document.querySelector('.block-carousel__wrapper');
-  var $slidesToShow = carousel.getAttribute("data-num-slides");
-  if ($slidesToShow == "") $slidesToShow = 1;
-  var $slidesToScroll = carousel.getAttribute("data-num-advance");
-  if ($slidesToScroll == "") $slidesToScroll = 1;
-  var $breakpoints = carousel.getAttribute("data-breakpoints");
-  var $responsive_obj = null;
+  var carouselWrapper = document.querySelectorAll('.block-carousel__wrapper');
+  carouselWrapper.forEach(function (carousel) {
+    //let carousel = document.querySelector('.block-carousel__wrapper');
+    var $slidesToShow = carousel.getAttribute("data-num-slides");
+    if ($slidesToShow == "") $slidesToShow = 1;
+    var $slidesToScroll = carousel.getAttribute("data-num-advance");
+    if ($slidesToScroll == "") $slidesToScroll = 1;
+    var $breakpoints = carousel.getAttribute("data-breakpoints");
+    var $responsive_obj = null;
 
-  if ($breakpoints != "") {
-    $responsive_obj = JSON.parse(decodeURIComponent($breakpoints));
-  } //console.log("slidesToShow", $slidesToShow);
-  //console.log("slidesToScroll", $slidesToScroll);
-  //console.log("responsive_obj", $responsive_obj);
-
-
-  new Glider(document.querySelector('.wp-block-acf-glider-carousel'), {
-    slidesToShow: $slidesToShow,
-    slidesToScroll: $slidesToScroll,
-    arrows: {
-      prev: '.block-carousel__prev',
-      next: '.block-carousel__next'
-    },
-    dots: '.block-carousel__dots',
-    responsive: $responsive_obj
-    /* example
-    responsive: [
-    {
-      // screens greater than >= 775px
-      breakpoint: 775,
-      settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 'auto',
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25
-      }
-    },{
-      // screens greater than >= 1024px
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        itemWidth: 150,
-        duration: 0.25
-      }
+    if ($breakpoints != "") {
+      $responsive_obj = JSON.parse(decodeURIComponent($breakpoints));
     }
-    ]
-    */
 
+    var $id = carousel.getAttribute("data-id"); //console.log("slidesToShow", $slidesToShow);
+    //console.log("slidesToScroll", $slidesToScroll);
+    //console.log("responsive_obj", $responsive_obj);
+
+    new Glider(carousel.querySelector('.wp-block-acf-glider-carousel'), {
+      slidesToShow: $slidesToShow,
+      slidesToScroll: $slidesToScroll,
+      arrows: {
+        prev: '.glider-prev-' + $id,
+        next: '.glider-next-' + $id
+      },
+      dots: '.glider-dots-' + $id,
+      responsive: $responsive_obj
+      /* example
+      responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 'auto',
+          slidesToScroll: 'auto',
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+      ]
+      */
+
+    });
   });
 });
