08bc7b3d695dce90a2a2508ba441fd4e8ae4d3cc
Andy Schaff <andy@portent.com>
2021-10-06 12:51:18 -0500

    fixing logic for session storage handling and hidden form field inputs

--- wp-content/themes/portent_enercare_2021/assets/js/src/global.js
+++ wp-content/themes/portent_enercare_2021/assets/js/src/global.js
@@ -16,101 +16,118 @@ jQuery(function($){
 		$('.search-toggle, .header-search').toggleClass('active');
 		$('.site-header .search-field').focus();
 	});
-  
-  /**
-	 * Array of url parameters structured with the key
-	 * as the key we will store/pull from sessionStorage
-	 * and the value as a string of what url parameter we
-	 * will need to pull the data from.
-	 */
-	window.Enercare.PPCparams = [
-    { utm_source: 'utm_source' },
-    { utm_medium: 'utm_medium' },
-		{ utm_campaign: 'utm_campaign' },
-    /*
-		{ Keyword: 'keyword' },
-		{ Campaign: 'cid' },
-		{ AdGroup: 'aid' },
-    { GoogleCookieID: 'gclid' },
-		{ ReferralCode: 'refcode' },
-    { DeviceType: '' }
-    */
-	];
-
-
-	/**
-	 * Cookie value grab for closed loop analytics in Google Tag Manager.
-	 * Imlemented via portent.
-	 * @param cname
-	 * @returns {string}
-	 */
-	function getCookie(cname) {
-		var name = cname + "=";
-		var decodedCookie = decodeURIComponent(document.cookie);
-		var ca = decodedCookie.split(';');
-		for(var i = 0; i <ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return "";
-	}
 
   //Enercare Specific function
-	(function (window,document, undefined) {
+  (function (window,document, undefined) {
+    
+    window.Enercare = {};
+    
+    /**
+     * Array of url parameters structured with the key
+     * as the key we will store/pull from sessionStorage
+     * and the value as a string of what url parameter we
+     * will need to pull the data from.
+     */
+    window.Enercare.PPCparams = [
+      { utm_source: 'utm_source' },
+      { utm_medium: 'utm_medium' },
+      { utm_campaign: 'utm_campaign' },
+      /*
+      { Keyword: 'keyword' },
+      { Campaign: 'cid' },
+      { AdGroup: 'aid' },
+      { GoogleCookieID: 'gclid' },
+      { ReferralCode: 'refcode' },
+      { DeviceType: '' }
+      */
+    ];
 
-		window.Enercare.getUrlParameter = function(name) {
-			const n = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
-			const regex = new RegExp(`[\\?&]${n}=([^&#]*)`);
-			const results = regex.exec(location.search);
-			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-		};
 
-		window.Enercare.logPPCParamsToStorage = () => {
-			window.Enercare.PPCparams.forEach(param => {
-				const key = Object.keys(param)[0];
-				const urlParam = param[key];
+    /**
+     * Cookie value grab for closed loop analytics in Google Tag Manager.
+     * Imlemented via portent.
+     * @param cname
+     * @returns {string}
+     */
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
 
-				if (!sessionStorage[key]) {
-					sessionStorage[key] = window.Enercare.getUrlParameter(urlParam);
-				}
-			});
-		};
-  
+    window.Enercare.getUrlParameter = function(name) {
+      const n = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+      const regex = new RegExp(`[\\?&]${n}=([^&#]*)`);
+      const results = regex.exec(location.search);
+      return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
+
+    window.Enercare.logPPCParamsToStorage = () => {
+      window.Enercare.PPCparams.forEach(param => {
+        const key = Object.keys(param)[0];
+        const urlParam = param[key];
+
+        if (!sessionStorage[key]) {
+          sessionStorage[key] = window.Enercare.getUrlParameter(urlParam);
+        }
+      });
+    };
+
     window.Enercare.handleHiddenFormFields = function(form) {
-			// handle the VendorCookieID/ruid
-			if (form.VendorCookieID === undefined) {
-				var input = document.createElement("input");
-				input.setAttribute("type", "hidden");
-				input.setAttribute("name", "VendorCookieID");
-				input.setAttribute("value", getCookie('ruid'));
-				form.appendChild(input);
-			} else {
-				form.VendorCookieID.setAttribute("value", getCookie('ruid'));
-			}
+      // handle the VendorCookieID/ruid
+      if (form.VendorCookieID === undefined) {
+        var input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", "ruid");
+        input.setAttribute("value", getCookie('ruid'));
+        form.appendChild(input);
+      } else {
+        form.VendorCookieID.setAttribute("value", getCookie('ruid'));
+      }
 
-			// loop through our PPC params
-			window.Enercare.PPCparams.forEach(param => {
-				const key = Object.keys(param)[0];
+      // loop through our PPC params
+      window.Enercare.PPCparams.forEach(param => {
+        const key = Object.keys(param)[0];
 
-				// if form element does not exist, create it and append to form
-				if (form.key === undefined) {
-					var input = document.createElement("input");
-					input.setAttribute("type", "hidden");
-					input.setAttribute("name", key);
-					input.setAttribute("value", sessionStorage[key]);
-					form.appendChild(input);
-				} else {
-					// otherwise, update the value of the hidden field
-					form.key.setAttribute("value", sessionStorage[key]);
-				}
+        // if form element does not exist, create it and append to form
+        if (form.key === undefined) {
+          var input = document.createElement("input");
+          input.setAttribute("type", "hidden");
+          input.setAttribute("name", key);
+          input.setAttribute("value", sessionStorage[key]);
+          form.appendChild(input);
+        } else {
+          // otherwise, update the value of the hidden field
+          form.key.setAttribute("value", sessionStorage[key]);
+        }
 
-			});
-		}
-	})(window,document);
-
+      });
+    }
+    
+    /**
+     * Loop through PPCparams
+     * check to see if sessionStorage item exists for that key
+     * make sure theres no value, then push the value from the
+     * url parameter provided
+     */
+    window.Enercare.logPPCParamsToStorage();
+    
+    /** Get the gravity form and handle hidden form field inputs */
+    var gravityForm = $('.gform_wrapper form');
+    if (gravityForm.length) {
+      window.Enercare.handleHiddenFormFields(gravityForm[0]);
+    }
+    
+  })(window,document);
+  
 });
