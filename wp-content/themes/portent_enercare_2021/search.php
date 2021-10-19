f5695f4dcf6fd434af775a1a05d76c2c009f99e6
Andy Schaff <andy@portent.com>
2021-10-18 20:01:51 -0500

    breaking search template out into its own file. yanking search-specifics from other archive files

--- wp-content/themes/portent_enercare_2021/search.php
+++ wp-content/themes/portent_enercare_2021/search.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Archive
+ * Search
  *
  * @package      EAStarter
  * @author       Bill Erickson
@@ -21,6 +21,9 @@ function enercare_archive_body_class( $classes ) {
 }
 add_filter( 'body_class', 'enercare_archive_body_class' );
 
+// Breadcrumbs
+add_action( 'enercare_archive_header_before', 'enercare_breadcrumbs', 5 );
+
 /**
  * Archive Header
  *
@@ -29,28 +32,14 @@ function enercare_archive_header() {
 
 	$title = $subtitle = $description = $more = false;
 
-	if( is_home() && get_option( 'page_for_posts' ) ) {
-		$title = get_the_title( get_option( 'page_for_posts' ) );
-
-	} elseif( is_search() ) {
+	if( is_search() ) {
 		$title = 'Search Results';
 		$more = get_search_form( false );
-
-	} elseif( is_archive() ) {
-		$title = get_the_archive_title();
-		if( ! get_query_var( 'paged' ) )
-			$description = get_the_archive_description();
-	} elseif( is_post_type_archive( 'location' ) ) {
-		$title = 'Find an Enercare Location';
-		$description = 'Location archive text';
 	}
-
 	if( empty( $title ) && empty( $description ) )
 		return;
 
 	$classes = [ 'archive-description' ];
-	if( is_author() )
-		$classes[] = 'author-archive-description';
 
 	echo '<header class="' . join( ' ', $classes ) . '">';
 	do_action ('enercare_archive_header_before' );
@@ -58,8 +47,6 @@ function enercare_archive_header() {
 		echo '<h1 class="archive-title">' . $title . '</h1>';
 	if( !empty( $subtitle ) )
 		echo '<h4>' . $subtitle . '</h4>';
-
-	get_sidebar('offers-header');
 
 	echo apply_filters( 'enercare_the_content', $description );
 	echo $more;
@@ -71,22 +58,20 @@ function enercare_archive_header() {
 }
 add_action( 'tha_content_while_before', 'enercare_archive_header' );
 
-// Breadcrumbs
-add_action( 'enercare_archive_header_before', 'enercare_breadcrumbs', 5 );
-
-//Filters
-add_action( 'enercare_archive_header_after', 'the_postal_code_filter' );
-
-// add section wrapper -- we'll use this on our ajax calls to replace the results
-function enercare_archive_wrapper_top() {
-  echo '<section class="archive-wrapper">';
+function addSearch_embed() {
+	?>
+	<div class="addsearch-container">
+    <!-- Search results will be rendered to this div -->
+    <div id="addsearch-results"></div>
+    <!-- Script must be below search field and addsearch-results div -->
+    <script src="https://addsearch.com/js/?key=3145819e621ccfb6dbf5116b2c92967b&type=resultpage"></script>
+  </div>
+	<?php
 }
-add_action('tha_content_while_before', 'enercare_archive_wrapper_top', 99999);
 
-function enercare_archive_wrapper_bottom() {
-  echo '</section>';
+if (is_search()) {
+	add_action('tha_content_bottom', 'addSearch_embed');
 }
-add_action('tha_content_bottom', 'enercare_archive_wrapper_bottom');
 
 // Build the page
 require get_template_directory() . '/index.php';
