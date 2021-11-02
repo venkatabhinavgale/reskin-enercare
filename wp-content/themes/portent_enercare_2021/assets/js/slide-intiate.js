31f4d0766bc02bdcd854a560e105a84965c99fe0
Branden Youngs <hello@brandenbuilds.com>
2021-11-02 13:49:01 -0400

    bug: adjusting CSS so always slides up after closing

--- wp-content/themes/portent_enercare_2021/assets/js/slide-intiate.js
+++ wp-content/themes/portent_enercare_2021/assets/js/slide-intiate.js
@@ -1,25 +1,24 @@
 "use strict";
 
-document.addEventListener(
-    "DOMContentLoaded", () => {
-        const menu = new MmenuLight(
-            document.querySelector( "#slider-menu" ),
-            "(max-width: 768px)"
-        );
+document.addEventListener("DOMContentLoaded", () => {
+	const menu = new MmenuLight(
+		document.querySelector("#slider-menu"),
+		"(max-width: 768px)"
+	);
 
-        const body = document.querySelector( "body");
+	const body = document.querySelector("body");
 
-        const navigator = menu.navigation();
-        const drawer = menu.offcanvas();
+	const navigator = menu.navigation();
+	const drawer = menu.offcanvas();
 
-        document.querySelector( "a[href='#slider-menu']" )
-            .addEventListener( "click", ( evnt ) => {
-                evnt.preventDefault();
-                if(body.classList.contains('mm-ocd-opened')) {
-                    drawer.close();
-                } else {
-                    drawer.open();
-                }
-            });
-    }
-);
+	document
+		.querySelector("a[href='#slider-menu']")
+		.addEventListener("click", (evnt) => {
+			evnt.preventDefault();
+			if (body.classList.contains("mm-ocd-opened")) {
+				drawer.close();
+			} else {
+				drawer.open();
+			}
+		});
+});
