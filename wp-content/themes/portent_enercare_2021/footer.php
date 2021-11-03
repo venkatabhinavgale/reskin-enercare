cd6e4fe8a98452229f23122b162e37e7ebf4ca16
Andy Schaff <andy@portent.com>
2021-11-02 19:56:11 -0500

    adding cl-phone class to anchor tags in main phone CTA areas. main nav, floating footer cta. updating copyright/terms to match comp

--- wp-content/themes/portent_enercare_2021/footer.php
+++ wp-content/themes/portent_enercare_2021/footer.php
@@ -11,7 +11,11 @@
 function footer_cta() {
 	if( get_field('floating_cta_link', 'options') ) {
 		$footer_link = get_field( 'floating_cta_link', 'options');
-		echo '<div data-interface="floating-cta" class="floating-cta is-style-shadowed"><a class="wp-block-button__link has-red-background-color has-background" href="' . $footer_link['url']. '"> ' . $footer_link['title']. '</a></div>';
+    $cl_phone = "";
+    if (strpos($footer_link, 'tel:') !== false) {
+      $cl_phone = " cl-phone";
+    }
+		echo '<div data-interface="floating-cta" class="floating-cta is-style-shadowed"><a class="wp-block-button__link has-red-background-color has-background' . $cl_phone . '" href="' . $footer_link['url']. '"> ' . $footer_link['title']. '</a></div>';
 	}
 }
 add_action( 'wp_footer', 'footer_cta' );
