efc32d75639d3e32b9d9f879db349d7c7736a6b5
Jeremiah Bratton <jeremiahbratton@me.com>
2021-11-06 15:29:57 -0700

    Setting up classes for faq block and adjusting classes in block editor to appropriately display wide vs normal widths

--- wp-content/themes/portent_enercare_2021/partials/blocks/block-faqs.php
+++ wp-content/themes/portent_enercare_2021/partials/blocks/block-faqs.php
@@ -12,7 +12,12 @@ $heading = get_field( 'header' );
 $content = get_field( 'content' );
 $faqs = get_field( 'faqs' );
 
-echo '<div class="block-faqs block-faqs--tablet--'.$tablet_appearance.'" data-allow-multiple>';
+$classes = 'block-faqs';
+if( !empty($block['className']) ) {
+	$classes .= sprintf( ' %s', $block['className'] );
+}
+
+echo '<div class="' . $classes . '" data-allow-multiple>';
 
 if( !empty( $heading ) )
 	echo '<h3 class="block-faqs--header">' . esc_html( $heading ) . '</h3>';
@@ -23,11 +28,11 @@ if( !empty( $faqs ) )  {
   foreach ($faqs as $faq) {
     echo '<div class="block-faqs--faq-container" aria-controls="faq_' . $faq->ID . '">';
     echo esc_html($faq->post_title);
-    
+
     echo '<div class="block-faqs--faq-answer-container" aria-expanded="false" data-state="closed" aria-labelledby="faq_' . $faq->ID . '">';
     echo $faq->post_content;
     echo '</div>';
-    
+
     echo '</div>';
   }
 }
