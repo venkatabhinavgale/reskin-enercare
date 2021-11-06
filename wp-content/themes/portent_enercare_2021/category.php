8d103a71f91c8e03a2deed3dab17033e9c4c1208
Jeremiah Bratton <jeremiahbratton@me.com>
2021-11-06 13:54:29 -0700

    comments and additional DRYing

--- wp-content/themes/portent_enercare_2021/category.php
+++ wp-content/themes/portent_enercare_2021/category.php
@@ -17,6 +17,11 @@ add_filter( 'enercare_page_layout', 'enercare_return_full_width_content' );
  */
 function enercare_archive_body_class( $classes ) {
 	$classes[] = 'archive';
+
+	//Place a taxonomy body class to ease with styling and remove the need to specificity on which term or taxonomy is being used
+	if(is_tax() ) {
+		$classes[] = 'taxonomy';
+	}
 	return $classes;
 }
 add_filter( 'body_class', 'enercare_archive_body_class' );
@@ -34,7 +39,7 @@ add_action( 'tha_content_while_before', 'enercare_archive_header' );
 // Breadcrumbs
 add_action( 'enercare_archive_header_after', 'enercare_breadcrumbs', 5 );
 
-// add section wrapper -- we'll use this on our ajax calls to replace the results
+// add section wrapper -- we'll use this on our ajax calls to replace the results, yes the name sucks... roll with it.
 function enercare_archive_wrapper_top() {
 	echo '<div class="archive-containment-field">';
     echo '<section class="archive-wrapper">';
