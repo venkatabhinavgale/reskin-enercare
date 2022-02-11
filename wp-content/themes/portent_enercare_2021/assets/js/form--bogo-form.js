dd514577f7d517e10d7bff70014bc0280f79779c
Jeremiah Bratton <jeremiahbratton@me.com>
2022-02-10 16:18:52 -0800

    Finishing up the 1 hour change that took 4 hours to do

--- wp-content/themes/portent_enercare_2021/assets/js/form--bogo-form.js
+++ wp-content/themes/portent_enercare_2021/assets/js/form--bogo-form.js
@@ -1,14 +1,45 @@
 "use strict";
 
-(function ($) {
-  console.log('omg');
-  var testing = document.querySelector('fieldset.radio__boiler-furnace');
-  console.log(testing);
-  $(document).ready(function () {
+(function ($, document) {
+  function bogoFormPresentation() {
     var boilerFurnaceFieldset = $('fieldset.radio__boiler-furnace');
     var furnaceRadios = boilerFurnaceFieldset.find('input[value=Furnace][type=radio]');
     var boilerRadios = boilerFurnaceFieldset.find('input[value=Boiler][type=radio]');
-    $('<img alt="" role="presentation" width=90 height=90 src="/wp-content/uploads/2021/10/furnace-24px-b.svg"/>').insertBefore(furnaceRadios);
-    $('<img alt="" role="presentation" width=90 height=90 src="/wp-content/uploads/2021/10/boiler-24px-b.svg"/>').insertBefore(boilerRadios);
+    var choiceLabels = boilerFurnaceFieldset.find('.gchoice label');
+    var choiceInputs = boilerFurnaceFieldset.find('input');
+
+    var inputChanges = function inputChanges(target) {
+      $('.gchoice.selected').removeClass('selected');
+
+      if (target.parent().hasClass('selected')) {
+        target.parent().removeClass('selected');
+      } else {
+        target.parent().addClass('selected');
+      }
+    };
+
+    var setSelect = function setSelect(target) {
+      console.log(target);
+
+      if (target[0].checked) {
+        target.parent().addClass('selected');
+      }
+    };
+
+    choiceInputs.each(function () {
+      setSelect($(this));
+    });
+    choiceInputs.on('change', function () {
+      inputChanges($(this));
+    });
+    choiceLabels.on('click', function (event) {
+      inputChanges($(this));
+    });
+    $('<img alt="" role="presentation" width=60 height=60 src="/wp-content/uploads/2021/10/furnace-24px-b.svg"/>').insertBefore(furnaceRadios);
+    $('<img alt="" role="presentation" width=60 height=60 src="/wp-content/uploads/2021/10/boiler-24px-b.svg"/>').insertBefore(boilerRadios);
+  }
+
+  $(document).on('gform_post_render', function () {
+    bogoFormPresentation();
   });
-})(jQuery);
+})(jQuery, document);
