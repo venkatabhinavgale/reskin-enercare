a266275b6caff86b369dabee23932cdd8c3d572f
Jeremiah Bratton <jeremiahbratton@me.com>
2022-03-11 14:32:18 -0800

    Completing initial integration for tabbed content anchors

--- wp-content/plugins/portent-tabbed-content/src/block-tabs/block.js
+++ wp-content/plugins/portent-tabbed-content/src/block-tabs/block.js
@@ -17,9 +17,15 @@ import { InnerBlocks, useBlockProps, AlignmentToolbar, BlockControls } from '@wo
 
 const ALLOWED_BLOCKS = [ 'portent/block-portent-tabbed-content--tab' ];
 
+const setAnchor = (anchorString) => {
+	let newAnchorNoSpecial = anchorString.replace(/&|\s*/g, '');
+	newAnchorNoSpecial = newAnchorNoSpecial.replace(/[^a-zA-Z0-9]/, '').toLowerCase();
+	return newAnchorNoSpecial;
+}
+
 const PrintTabs = (props) => {
 	const tabs = props.tabs;
-	return(tabs.map(tab => <button className="block-tabbed-content__tab" data-default={tab[3]} data-anchor={tab[4]} data-interface="tab-button" data-tab={tab[0]}><img width="30px" height="30px" alt="" src={tab[2]}/>{tab[1]}</button>));
+	return(tabs.map(tab => <button className="block-tabbed-content__tab" data-default={tab[3]} data-anchor={setAnchor(tab[1])+'-tab'} data-interface="tab-button" data-tab={tab[0]}><img width="30px" height="30px" alt="" src={tab[2]}/>{tab[1]}</button>));
 }
 
 const ChildTabs = (props) => {
