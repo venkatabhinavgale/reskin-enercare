f5bdb7c78c9755971b2a01d5a34ec3028053a799
Andy Schaff <andy@portent.com>
2022-04-04 13:19:16 -0500

    adding IP blocking code

--- wp-config.php
+++ wp-config.php
@@ -48,6 +48,34 @@ if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['P
 	define('NONCE_SALT',       'put your unique phrase here');
 }
 
+// IPv4: Single IPs and CIDR.
+// See https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
+$request_ip_blocklist = [
+  '45.9.20.89',
+];
+
+$request_remote_addr = $_SERVER['REMOTE_ADDR'];
+// Check if this IP is in blocklist.
+if (!$request_ip_forbidden = in_array($request_remote_addr, $request_ip_blocklist)) {
+  // Check if this IP is in CIDR block list.
+  foreach ($request_ip_blocklist as $_cidr) {
+    if (strpos($_cidr, '/') !== FALSE) {
+      $_ip = ip2long($request_remote_addr);
+      list ($_net, $_mask) = explode('/', $_cidr, 2);
+      $_ip_net = ip2long($_net);
+      $_ip_mask = ~((1 << (32 - $_mask)) - 1);
+
+      if ($request_ip_forbidden = ($_ip & $_ip_mask) == ($_ip_net & $_ip_mask)) {
+        break;
+      }
+    }
+  }
+}
+
+if ($request_ip_forbidden) {
+  header('HTTP/1.0 403 Forbidden');
+  exit;
+}
 
 /** Standard wp-config.php stuff from here on down. **/
 
