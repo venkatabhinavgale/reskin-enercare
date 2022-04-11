8457a1685742fa8951909bbb7c0821d0c09d9ff1
Jeremiah Bratton <jeremiahbratton@me.com>
2022-04-11 13:37:57 -0700

    Code update for toggle menu

--- wp-content/themes/portent_enercare_2021/assets/js/global.js
+++ wp-content/themes/portent_enercare_2021/assets/js/global.js
@@ -193,42 +193,219 @@ jQuery(function ($) {
  * Toggle Navigation Script
  */
 
-window.addEventListener('load', setupToggleNav);
+var PortentToggleNav = function PortentToggleNav() {};
 
-function setupToggleNav() {
-  var primaryNavigation = document.getElementById('primary-menu');
-  navigationMenuToggle(primaryNavigation);
+PortentToggleNav.prototype.menu = '';
+
+PortentToggleNav.prototype.init = function () {
+  if (this.menu !== '') {
+    /*
+    Add a class for style tracking
+     */
+    this.menu.classList.add('portent-toggle-nav');
+    this.setupMobileToggle();
+    this.setupStatusArea();
+    this.navigationMenuToggle(this.menu);
+    this.setupClickOutside();
+  } else {
+    console.log('Please set a menu property');
+  }
+};
+
+PortentToggleNav.prototype.setupClickOutside = function () {
   /**
    * Handle Closing the menu when a user clicks outside of the frame
    */
+  //THIS gets weird once we start to dive down into functions. To make sure we are calling the top level prototype when we ask for properties and functions we need to save top level this into a new variable.
+  var _this = this;
 
+  var navigationMenu = this.menu;
   document.addEventListener("click", function (event) {
-    var isNavigationInClick = primaryNavigation.contains(event.target);
+    var isNavigationInClick = navigationMenu.contains(event.target);
 
     if (!isNavigationInClick) {
-      closeAllMenus(primaryNavigation);
+      _this.closeAllMenus(navigationMenu);
     }
   });
-}
+};
 
-function closeAllMenus(navigationContainer) {
-  var openMenus = navigationContainer.querySelectorAll('button[aria-expanded=true]');
-  openMenus.forEach(function (elem) {
-    elem.parentNode.dataset.open = 'false';
-    elem.setAttribute('aria-expanded', "false");
+PortentToggleNav.prototype.setupStatusArea = function () {
+  var _this = this;
+
+  var menuElement = this.menu;
+  var statusAreaContainer = document.createElement('div');
+  var statusAreaBackButton = document.createElement('button');
+  var statusAreaCloseButton = document.createElement('button');
+  statusAreaContainer.setAttribute('aria-hidden', "true");
+  statusAreaContainer.setAttribute('data-interface', 'statusArea');
+  statusAreaCloseButton.innerText = 'Close';
+  statusAreaCloseButton.classList.add('mobile-close-btn');
+  statusAreaBackButton.innerText = 'Back to main menu';
+  statusAreaBackButton.classList.add('mobile-back-btn');
+  statusAreaContainer.appendChild(statusAreaBackButton);
+  statusAreaContainer.appendChild(statusAreaCloseButton);
+  menuElement.insertBefore(statusAreaContainer, menuElement.children[0]);
+  statusAreaBackButton.addEventListener("click", function () {
+    _this.closeAllMenus(menuElement);
   });
-}
+  statusAreaCloseButton.addEventListener("click", function () {
+    _this.closeMobileMenu(menuElement);
+  });
+};
 
-function focusFirstOption(topLevelItem) {
-  var firstLink = topLevelItem.querySelector('a');
-  firstLink.focus();
-}
+PortentToggleNav.prototype.setupMobileToggle = function () {
+  var _this = this;
+
+  var menuElement = this.menu;
+  var mobileMenuToggle;
+
+  if (this.toggleButton) {
+    mobileMenuToggle = this.toggleButton;
+  } else {
+    mobileMenuToggle.innerText = 'Menu';
+    mobileMenuToggle = document.createElement('button');
+    menuElement.parentElement.insertBefore(mobileMenuToggle, menuElement);
+  }
+
+  mobileMenuToggle.addEventListener("click", function () {
+    _this.openMobileMenu(_this.menu);
+  });
+  mobileMenuToggle.setAttribute('aria-expanded', 'false');
+};
+/**
+ * This is likely better handled through a dispatched event listener
+ * @param menuState
+ */
+
+
+PortentToggleNav.prototype.setSubMenuStatus = function () {
+  var menuState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+
+  if (menuState) {
+    this.menu.setAttribute('data-menu', 'open');
+  } else {
+    this.menu.removeAttribute('data-menu');
+  }
+};
+
+PortentToggleNav.prototype.navigationMenuToggle = function (navigationContainer) {
+  var menuItems = navigationContainer.querySelectorAll('[data-children=true]'); //THIS gets weird once we start to dive down into functions. To make sure we are calling the top level prototype when we ask for properties and functions we need to save top level this into a new variable.
+
+  var _this = this;
+
+  menuItems.forEach.call(menuItems, function (el, i) {
+    el.querySelector('button').addEventListener("click", function (event) {
+      if (this.parentNode.dataset.open === 'true') {
+        /**
+         * If the same menu button we are clicking on is already visible then toggle it closed
+         * @type {string}
+         */
+        this.parentNode.dataset.open = 'false';
+        this.setAttribute('aria-expanded', "false");
+
+        _this.setSubMenuStatus();
+      } else {
+        /**
+         * If the button we are clicking on is not open, reset all open menus before displaying the current option
+         */
+        _this.closeAllMenus(navigationContainer);
+
+        this.parentNode.dataset.open = 'true';
+        this.setAttribute('aria-expanded', "true");
+
+        _this.setSubMenuStatus(true);
+      }
+      /**
+       * Focus The First Item
+       */
+
+
+      _this.focusFirstOption(this.parentNode);
+
+      return false;
+    });
+    el.querySelector('button').addEventListener("keydown", function (event) {
+      console.log(event);
+
+      if (event.keyCode === 40) {
+        event.preventDefault();
+        console.log('Key code 40');
+        /**
+         * @todo This is a duplication of the opening menu logic. Condense this.
+         */
+
+        if (event.target.parentNode.dataset.open === 'true') {
+          /**
+           * If the same menu button we are clicking on is already visible then toggle it closed
+           * @type {string}
+           */
+          event.target.dataset.open = 'false';
+          event.target.setAttribute('aria-expanded', "false");
+
+          _this.setSubMenuStatus(false);
+        } else {
+          /**
+           * If the button we are clicking on is not open, reset all open menus before displaying the current option
+           */
+          _this.closeAllMenus(navigationContainer);
+
+          event.target.parentNode.dataset.open = 'true';
+          event.target.setAttribute('aria-expanded', "true");
+
+          _this.setSubMenuStatus(true);
+          /**
+           * Focus The First Item
+           */
+
+
+          _this.focusFirstOption(this.parentNode);
+        }
+      }
+
+      if (event.keyCode === 38) {
+        console.log('Key code 38');
+
+        _this.closeAllMenus(navigationContainer);
+      }
+
+      return false;
+    });
+    /**
+     * Event Listeners for ALL link elements within the current top level list item
+     * We are tracking all links within the top level container so that when links are buried in containers
+     * and out of sequence we can still locate the next logical link in the list.
+     */
+
+    el.querySelectorAll('a').forEach(function (elem) {
+      console.log('link for each');
+      elem.addEventListener('keydown', function (event) {
+        console.log('link key press');
+
+        if (event.keyCode === 40 || event.keyCode === 38) {
+          console.log('arrow press');
+          event.stopPropagation();
+          event.preventDefault();
+
+          _this.findNextMenuLink(event, el);
+        }
+      });
+      elem.addEventListener('keyup', function (event) {
+        //Close menu and focus on closest button when Escape is pressed on a menu item
+        if (event.keyCode === 27) {
+          _this.closeAllMenus(navigationContainer);
+
+          el.querySelector('button').focus();
+        }
+      });
+    });
+  });
+};
 /*
 This function essentially serves as a keyboard trap for the current menu.
  */
 
 
-function findNextMenuLink(event, topLevelParent) {
+PortentToggleNav.prototype.findNextMenuLink = function (event, topLevelParent) {
   var allLinks = topLevelParent.querySelectorAll('a');
   var firstFocusableLink = allLinks[0];
   var lastFocusableLink = allLinks[allLinks.length - 1];
@@ -267,94 +444,56 @@ function findNextMenuLink(event, topLevelParent) {
       }
     }
   });
-}
+};
 
-function navigationMenuToggle(navigationContainer) {
-  var menuItems = navigationContainer.querySelectorAll('[data-children=true]');
-  Array.prototype.forEach.call(menuItems, function (el, i) {
-    el.querySelector('button').addEventListener("click", function (event) {
-      if (this.parentNode.dataset.open === 'true') {
-        /**
-         * If the same menu button we are clicking on is already visible then toggle it closed
-         * @type {string}
-         */
-        this.parentNode.dataset.open = 'false';
-        this.setAttribute('aria-expanded', "false");
-      } else {
-        /**
-         * If the button we are clicking on is not open, reset all open menus before displaying the current option
-         */
-        closeAllMenus(navigationContainer);
-        this.parentNode.dataset.open = 'true';
-        this.setAttribute('aria-expanded', "true");
-      }
-      /**
-       * Focus The First Item
-       */
-
-
-      focusFirstOption(this.parentNode);
-      return false;
-    });
-    el.querySelector('button').addEventListener("keyup", function (event) {
-      if (event.keyCode === 40) {
-        event.preventDefault();
-        /**
-         * @todo This is a duplication of the opening menu logic. Condense this.
-         */
-
-        if (this.parentNode.dataset.open === 'true') {
-          /**
-           * If the same menu button we are clicking on is already visible then toggle it closed
-           * @type {string}
-           */
-          this.parentNode.dataset.open = 'false';
-          this.setAttribute('aria-expanded', "false");
-        } else {
-          /**
-           * If the button we are clicking on is not open, reset all open menus before displaying the current option
-           */
-          closeAllMenus(navigationContainer);
-          this.parentNode.dataset.open = 'true';
-          this.setAttribute('aria-expanded', "true");
-          /**
-           * Focus The First Item
-           */
-
-          focusFirstOption(this.parentNode);
-        }
-      }
-
-      if (event.keyCode === 38) {
-        closeAllMenus(navigationContainer);
-      }
-
-      return false;
-    }, true);
-    /**
-     * Event Listeners for ALL link elements within the current top level list item
-     * We are tracking all links within the top level container so that when links are buried in containers
-     * and out of sequence we can still locate the next logical link in the list.
-     */
-
-    el.querySelectorAll('a').forEach(function (elem) {
-      elem.addEventListener('keydown', function (event) {
-        if (event.keyCode === 40 || event.keyCode === 38) {
-          event.stopPropagation();
-          event.preventDefault();
-          findNextMenuLink(event, el);
-        }
-      });
-      elem.addEventListener('keyup', function (event) {
-        //Close menu and focus on closest button when Escape is pressed on a menu item
-        if (event.keyCode === 27) {
-          closeAllMenus(navigationContainer);
-          el.querySelector('button').focus();
-        }
-      });
-    });
+PortentToggleNav.prototype.closeAllMenus = function (navigationContainer) {
+  var openMenus = navigationContainer.querySelectorAll('button[aria-expanded=true]');
+  openMenus.forEach(function (elem) {
+    elem.parentNode.dataset.open = 'false';
+    elem.setAttribute('aria-expanded', "false");
   });
+  this.setSubMenuStatus(false);
+};
+
+PortentToggleNav.prototype.openMobileMenu = function (menuElement) {
+  menuElement.setAttribute('data-mobile', 'open');
+  var firstMenuItem = this.menu.querySelector('.menu-item button');
+  firstMenuItem.focus();
+};
+
+PortentToggleNav.prototype.closeMobileMenu = function () {
+  this.closeAllMenus(this.menu);
+
+  if (this.menu.getAttribute('data-mobile') === 'open') {
+    this.menu.removeAttribute('data-mobile');
+  }
+
+  this.setSubMenuStatus(false);
+};
+
+PortentToggleNav.prototype.focusFirstOption = function (topLevelItem) {
+  console.log('focusing first option');
+  console.log(topLevelItem);
+  var firstLink = topLevelItem.querySelector('.sub-menu a');
+  console.log(firstLink);
+  firstLink.focus({
+    preventScroll: true
+  });
+  console.log(document.activeElement);
+};
+/**
+ * Setup and init the toggle menu
+ */
+
+
+function setupToggleNav() {
+  var primaryNavigation = new PortentToggleNav();
+  primaryNavigation.menu = document.getElementById('slider-menu');
+  primaryNavigation.toggleButton = document.getElementById('slider-menu-toggle');
+  primaryNavigation.init();
 }
+
+window.addEventListener('load', setupToggleNav);
 "use strict";
 
 jQuery(function ($) {
--- wp-content/themes/portent_enercare_2021/assets/js/src/global.js
+++ wp-content/themes/portent_enercare_2021/assets/js/src/global.js
@@ -196,40 +196,198 @@ jQuery(function($){
 /**
  * Toggle Navigation Script
  */
-window.addEventListener('load', setupToggleNav);
+let PortentToggleNav = function(){};
+PortentToggleNav.prototype.menu = '';
+PortentToggleNav.prototype.init = function() {
 
-function setupToggleNav() {
-	const primaryNavigation = document.getElementById('primary-menu');
-	navigationMenuToggle(primaryNavigation);
+	if(this.menu !== '') {
+		/*
+		Add a class for style tracking
+		 */
+		this.menu.classList.add('portent-toggle-nav');
+		this.setupMobileToggle();
+		this.setupStatusArea();
+		this.navigationMenuToggle(this.menu);
+		this.setupClickOutside();
+	} else {
+		console.log('Please set a menu property');
+	}
 
+};
+PortentToggleNav.prototype.setupClickOutside = function() {
 	/**
 	 * Handle Closing the menu when a user clicks outside of the frame
 	 */
+
+		//THIS gets weird once we start to dive down into functions. To make sure we are calling the top level prototype when we ask for properties and functions we need to save top level this into a new variable.
+	let _this = this;
+	let navigationMenu = this.menu;
 	document.addEventListener("click", function(event) {
-		let isNavigationInClick = primaryNavigation.contains(event.target)
+		let isNavigationInClick = navigationMenu.contains(event.target);
 		if(!isNavigationInClick) {
-			closeAllMenus(primaryNavigation);
+			_this.closeAllMenus(navigationMenu);
 		}
 	});
-}
+};
 
-function closeAllMenus(navigationContainer) {
-	let openMenus = navigationContainer.querySelectorAll('button[aria-expanded=true]');
-	openMenus.forEach(function(elem) {
-		elem.parentNode.dataset.open = 'false';
-		elem.setAttribute('aria-expanded', "false");
+PortentToggleNav.prototype.setupStatusArea = function() {
+	const _this = this;
+	const menuElement = this.menu;
+	const statusAreaContainer = document.createElement('div');
+	const statusAreaBackButton = document.createElement('button');
+	const statusAreaCloseButton = document.createElement('button');
+
+	statusAreaContainer.setAttribute('aria-hidden', "true");
+	statusAreaContainer.setAttribute('data-interface', 'statusArea');
+
+	statusAreaCloseButton.innerText = 'Close';
+	statusAreaCloseButton.classList.add('mobile-close-btn');
+	statusAreaBackButton.innerText = 'Back to main menu';
+	statusAreaBackButton.classList.add('mobile-back-btn');
+
+	statusAreaContainer.appendChild(statusAreaBackButton);
+	statusAreaContainer.appendChild(statusAreaCloseButton);
+	menuElement.insertBefore(statusAreaContainer, menuElement.children[0]);
+
+	statusAreaBackButton.addEventListener("click", function(){_this.closeAllMenus(menuElement);});
+	statusAreaCloseButton.addEventListener("click", function(){_this.closeMobileMenu(menuElement);});
+};
+
+PortentToggleNav.prototype.setupMobileToggle = function() {
+	const _this = this;
+	const menuElement = this.menu;
+
+	let mobileMenuToggle;
+	if(this.toggleButton) {
+		mobileMenuToggle = this.toggleButton;
+	} else {
+		mobileMenuToggle.innerText = 'Menu';
+		mobileMenuToggle = document.createElement('button');
+		menuElement.parentElement.insertBefore(mobileMenuToggle, menuElement);
+	}
+
+	mobileMenuToggle.addEventListener("click", function(){_this.openMobileMenu(_this.menu);});
+	mobileMenuToggle.setAttribute('aria-expanded', 'false');
+};
+
+/**
+ * This is likely better handled through a dispatched event listener
+ * @param menuState
+ */
+PortentToggleNav.prototype.setSubMenuStatus = function(menuState= false) {
+	if(menuState) {
+		this.menu.setAttribute('data-menu', 'open');
+	} else {
+		this.menu.removeAttribute('data-menu');
+	}
+};
+
+PortentToggleNav.prototype.navigationMenuToggle = function(navigationContainer) {
+	let menuItems = navigationContainer.querySelectorAll('[data-children=true]');
+
+	//THIS gets weird once we start to dive down into functions. To make sure we are calling the top level prototype when we ask for properties and functions we need to save top level this into a new variable.
+	let _this = this;
+	menuItems.forEach.call(menuItems, function(el, i){
+		el.querySelector('button').addEventListener("click",  function(event){
+			if (this.parentNode.dataset.open === 'true') {
+				/**
+				 * If the same menu button we are clicking on is already visible then toggle it closed
+				 * @type {string}
+				 */
+				this.parentNode.dataset.open = 'false';
+				this.setAttribute('aria-expanded', "false");
+				_this.setSubMenuStatus();
+			} else {
+				/**
+				 * If the button we are clicking on is not open, reset all open menus before displaying the current option
+				 */
+				_this.closeAllMenus(navigationContainer);
+				this.parentNode.dataset.open = 'true';
+				this.setAttribute('aria-expanded', "true");
+				_this.setSubMenuStatus(true);
+			}
+
+			/**
+			 * Focus The First Item
+			 */
+			_this.focusFirstOption(this.parentNode);
+
+			return false;
+		});
+
+		el.querySelector('button').addEventListener("keydown", function(event) {
+			console.log(event);
+			if(event.keyCode === 40 ) {
+				event.preventDefault();
+				console.log('Key code 40');
+
+				/**
+				 * @todo This is a duplication of the opening menu logic. Condense this.
+				 */
+				if (event.target.parentNode.dataset.open === 'true') {
+					/**
+					 * If the same menu button we are clicking on is already visible then toggle it closed
+					 * @type {string}
+					 */
+					event.target.dataset.open = 'false';
+					event.target.setAttribute('aria-expanded', "false");
+					_this.setSubMenuStatus(false);
+				} else {
+					/**
+					 * If the button we are clicking on is not open, reset all open menus before displaying the current option
+					 */
+					_this.closeAllMenus(navigationContainer);
+					event.target.parentNode.dataset.open = 'true';
+					event.target.setAttribute('aria-expanded', "true");
+					_this.setSubMenuStatus(true);
+
+					/**
+					 * Focus The First Item
+					 */
+					_this.focusFirstOption(this.parentNode);
+				}
+			}
+
+			if(event.keyCode === 38) {
+				console.log('Key code 38');
+				_this.closeAllMenus(navigationContainer);
+			}
+
+			return false;
+		});
+
+		/**
+		 * Event Listeners for ALL link elements within the current top level list item
+		 * We are tracking all links within the top level container so that when links are buried in containers
+		 * and out of sequence we can still locate the next logical link in the list.
+		 */
+		el.querySelectorAll('a').forEach( function(elem) {
+			console.log('link for each');
+			elem.addEventListener('keydown', function(event) {
+				console.log('link key press');
+				if(event.keyCode === 40 || event.keyCode === 38) {
+					console.log('arrow press');
+					event.stopPropagation();
+					event.preventDefault();
+					_this.findNextMenuLink(event, el);
+				}
+			});
+
+			elem.addEventListener('keyup', function(event) {
+				//Close menu and focus on closest button when Escape is pressed on a menu item
+				if(event.keyCode === 27 ){
+					_this.closeAllMenus(navigationContainer);
+					el.querySelector('button').focus();
+				}
+			});
+		});
 	});
-}
-
-function focusFirstOption(topLevelItem) {
-	let firstLink = topLevelItem.querySelector('a');
-	firstLink.focus();
-}
+};
 
 /*
 This function essentially serves as a keyboard trap for the current menu.
  */
-function findNextMenuLink(event, topLevelParent) {
+PortentToggleNav.prototype.findNextMenuLink = function(event, topLevelParent) {
 	let allLinks = topLevelParent.querySelectorAll('a');
 	let firstFocusableLink = allLinks[0];
 	let lastFocusableLink = allLinks[allLinks.length - 1];
@@ -240,8 +398,8 @@ function findNextMenuLink(event, topLevelParent) {
 	 * It is important that we return if this statement is true so that we do not duplicate the action.
 	 */
 	if(currentElement === firstFocusableLink && event.keyCode === 38 ) {
-			lastFocusableLink.focus();
-			return;
+		lastFocusableLink.focus();
+		return;
 	}
 
 	/**
@@ -258,102 +416,59 @@ function findNextMenuLink(event, topLevelParent) {
 	 * then loop through the list until
 	 */
 	allLinks.forEach(function(link, index) {
-			if(currentElement === link ) {
-				if(event.keyCode === 40) {
-					allLinks[index+1].focus();
-				} else if(event.keyCode === 38 ){
-					allLinks[index-1].focus();
-				}
+		if(currentElement === link ) {
+			if(event.keyCode === 40) {
+				allLinks[index+1].focus();
+			} else if(event.keyCode === 38 ){
+				allLinks[index-1].focus();
 			}
-		});
-}
-
-function navigationMenuToggle(navigationContainer) {
-	let menuItems = navigationContainer.querySelectorAll('[data-children=true]');
-
-	Array.prototype.forEach.call(menuItems, function(el, i){
-		el.querySelector('button').addEventListener("click",  function(event){
-			if (this.parentNode.dataset.open === 'true') {
-				/**
-				 * If the same menu button we are clicking on is already visible then toggle it closed
-				 * @type {string}
-				 */
-				this.parentNode.dataset.open = 'false';
-				this.setAttribute('aria-expanded', "false");
-			} else {
-				/**
-				 * If the button we are clicking on is not open, reset all open menus before displaying the current option
-				 */
-				closeAllMenus(navigationContainer);
-				this.parentNode.dataset.open = 'true';
-				this.setAttribute('aria-expanded', "true");
-			}
-
-			/**
-			 * Focus The First Item
-			 */
-			focusFirstOption(this.parentNode);
-
-			return false;
-		});
-
-		el.querySelector('button').addEventListener("keyup", function(event) {
-			if(event.keyCode === 40 ) {
-				event.preventDefault();
-
-				/**
-				 * @todo This is a duplication of the opening menu logic. Condense this.
-				 */
-				if (this.parentNode.dataset.open === 'true') {
-					/**
-					 * If the same menu button we are clicking on is already visible then toggle it closed
-					 * @type {string}
-					 */
-					this.parentNode.dataset.open = 'false';
-					this.setAttribute('aria-expanded', "false");
-				} else {
-					/**
-					 * If the button we are clicking on is not open, reset all open menus before displaying the current option
-					 */
-					closeAllMenus(navigationContainer);
-					this.parentNode.dataset.open = 'true';
-					this.setAttribute('aria-expanded', "true");
-
-					/**
-					 * Focus The First Item
-					 */
-					focusFirstOption(this.parentNode);
-				}
-			}
-
-			if(event.keyCode === 38) {
-				closeAllMenus(navigationContainer);
-			}
-
-			return false;
-		}, true);
-
-		/**
-		 * Event Listeners for ALL link elements within the current top level list item
-		 * We are tracking all links within the top level container so that when links are buried in containers
-		 * and out of sequence we can still locate the next logical link in the list.
-		 */
-		el.querySelectorAll('a').forEach( function(elem) {
-			elem.addEventListener('keydown', function(event) {
-				if(event.keyCode === 40 || event.keyCode === 38) {
-					event.stopPropagation();
-					event.preventDefault();
-					findNextMenuLink(event, el);
-				}
-			});
-
-			elem.addEventListener('keyup', function(event) {
-				//Close menu and focus on closest button when Escape is pressed on a menu item
-				if(event.keyCode === 27 ){
-					closeAllMenus(navigationContainer);
-					el.querySelector('button').focus();
-				}
-			});
-		});
+		}
 	});
+};
+
+PortentToggleNav.prototype.closeAllMenus = function(navigationContainer) {
+	let openMenus = navigationContainer.querySelectorAll('button[aria-expanded=true]');
+	openMenus.forEach(function(elem) {
+		elem.parentNode.dataset.open = 'false';
+		elem.setAttribute('aria-expanded', "false");
+	});
+	this.setSubMenuStatus(false);
+};
+
+PortentToggleNav.prototype.openMobileMenu = function(menuElement) {
+	menuElement.setAttribute('data-mobile', 'open');
+	const firstMenuItem = this.menu.querySelector('.menu-item button');
+	firstMenuItem.focus();
+};
+
+PortentToggleNav.prototype.closeMobileMenu = function() {
+	this.closeAllMenus(this.menu);
+
+	if(this.menu.getAttribute('data-mobile') === 'open' ) {
+		this.menu.removeAttribute('data-mobile');
+	}
+
+	this.setSubMenuStatus(false);
+};
+
+PortentToggleNav.prototype.focusFirstOption = function(topLevelItem) {
+	console.log('focusing first option');
+	console.log(topLevelItem);
+	let firstLink = topLevelItem.querySelector('.sub-menu a');
+	console.log(firstLink);
+	firstLink.focus({preventScroll:true});
+	console.log(document.activeElement);
+};
+
+
+/**
+ * Setup and init the toggle menu
+ */
+function setupToggleNav() {
+	const primaryNavigation = new PortentToggleNav;
+	primaryNavigation.menu = document.getElementById('slider-menu');
+	primaryNavigation.toggleButton = document.getElementById('slider-menu-toggle');
+	primaryNavigation.init();
 }
+window.addEventListener('load', setupToggleNav);
+
