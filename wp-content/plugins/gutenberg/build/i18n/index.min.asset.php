95f6a8b2f80983e13cd84662ad82f55ad99bfcec
Andy Schaff <andy@portent.com>
2022-05-03 12:28:53 -0500

    removing the gutenburg plugin

--- wp-content/plugins/gutenberg/build/i18n/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-hooks', 'wp-polyfill'), 'version' => 'b0128b4d828f9033612bfa2fdb320afb');
--- wp-content/plugins/gutenberg/build/keycodes/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-i18n', 'wp-polyfill'), 'version' => '4ba73da93286735c096310b34470aacc');
