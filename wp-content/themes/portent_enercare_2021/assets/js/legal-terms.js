58e005fddbebb826be6adab8b31fc85cf97ce3f6
Jeremiah Bratton <jeremiahbratton@me.com>
2022-07-12 12:23:48 -0700

    Setting aria-expanded back to legal terms button

--- wp-content/themes/portent_enercare_2021/assets/js/legal-terms.js
+++ wp-content/themes/portent_enercare_2021/assets/js/legal-terms.js
@@ -7,12 +7,12 @@ window.addEventListener('load', function () {
       var termsContent = tc.querySelector('.legal__terms-details');
       var termsButton = tc.querySelector('.legal__terms-toggle');
 
-      if (termsContent.getAttribute("aria-expanded") == "true") {
-        termsContent.setAttribute("aria-expanded", "false");
+      if (termsButton.getAttribute("aria-expanded") == "true") {
+        termsButton.setAttribute("aria-expanded", "false");
         termsContent.setAttribute("data-state", "closed");
         termsButton.innerHTML = termsButton.innerHTML.replace("navigate-up", "navigate-down");
       } else {
-        termsContent.setAttribute("aria-expanded", "true");
+        termsButton.setAttribute("aria-expanded", "true");
         termsContent.setAttribute("data-state", "open");
         termsButton.innerHTML = termsButton.innerHTML.replace("navigate-down", "navigate-up");
       }
