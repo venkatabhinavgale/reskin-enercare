4d0fd31b8a3f38d758bd1fed35273e872767903b
Celina Gutierrez <cgutierrez@portent.com>
2022-12-07 15:52:51 -0800

    CKW-132 Button doesn't provide enough information - updated aria-labels across campaign T and C buttons to include text from visible label

--- wp-content/themes/portent_enercare_2021/partials/campaign-classic.php
+++ wp-content/themes/portent_enercare_2021/partials/campaign-classic.php
@@ -7,7 +7,7 @@
       <p class="block-offer-card__subheading"><?php echo $campaign_subheading; ?></p>
     <?php } ?>
     <h3 class="block-offer-card__heading"><?= $campaign_heading; ?></h3>
-    <?php if ($campaign_expiration && $campaign_expiration != "") { ?><p class="block-offer-card__offer-expiration">Offer expires <?= date('F d, Y', strtotime($campaign_expiration)); ?>.<?php } ?><button class="block-offer-card__terms-toggle" aria-controls="terms_<?= $campaign->ID ?>" data-micromodal-trigger="modal-<?=$campaign->ID ?>" aria-label="Open modal and view details and fine print for <?= $campaign_heading ?>">View Terms and Conditions</button></p>
+    <?php if ($campaign_expiration && $campaign_expiration != "") { ?><p class="block-offer-card__offer-expiration">Offer expires <?= date('F d, Y', strtotime($campaign_expiration)); ?>.<?php } ?><button class="block-offer-card__terms-toggle" aria-controls="terms_<?= $campaign->ID ?>" data-micromodal-trigger="modal-<?=$campaign->ID ?>" aria-label="Open modal and view Terms and Conditions for <?= $campaign_heading ?>">View Terms and Conditions</button></p>
     <a class="wp-block-button__link has-red-background-color has-background block-offer-card__link" href="<?= $campaign_destination ?>" aria-label="Get deal for <?= $campaign_heading; ?>"><img class="block-offer-card__link__icon" alt="" width="20" height="20" role="presentation" src="<?= $cta_icon ?>"/><span><?= $cta_text ?></span></a>
     <?= $campaign_terms; ?>
   </div>
