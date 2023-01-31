8df1a2418ac230fdf4e44a7c08f42b182764e55a
Celina Gutierrez <cgutierrez@portent.com>
2023-01-31 13:46:03 -0800

    Added lazy loading to google maps iframe

--- wp-content/themes/portent_enercare_2021/partials/blocks/block-location-map.php
+++ wp-content/themes/portent_enercare_2021/partials/blocks/block-location-map.php
@@ -11,7 +11,7 @@
 echo '<div class="block-location-map block-location-map--tablet--'.$tablet_appearance.'">';
 
 if (get_field('google_maps_url', get_the_ID())) {
-  echo '<iframe class="" src="' . get_field('google_maps_url', get_the_ID()) . '" width="568" height="400" style="border-radius: 2px; box-shadow: none; border: none;"></iframe>';
+  echo '<iframe class="" src="' . get_field('google_maps_url', get_the_ID()) . '" width="568" height="400" loading="lazy" style="border-radius: 2px; box-shadow: none; border: none;"></iframe>';
 }
 
 echo '</div>';
