2b7cde999b085473d9282084875cffea366f625a
Jeremiah Bratton <jeremiahbratton@me.com>
2023-07-28 13:42:43 -0700

    Newsletter block updates

--- wp-content/themes/portent_enercare_2021/partials/blocks/newsletter-signup/block-newsletter-signup.php
+++ wp-content/themes/portent_enercare_2021/partials/blocks/newsletter-signup/block-newsletter-signup.php
@@ -23,9 +23,11 @@ $gform = get_field('gravity_form');
 <section class="block-newsletter-form <?php echo esc_attr($classes); ?>" data-interface="block-newsletter-form">
 	<div class="block-newsletter-form__container">
 		<div class="block-newsletter-form__content-container">
-			<img data-interface="block-newsletter-form__image" class="block-newsletter-form__image" src="" />
-			<h2 data-interface="block-newsletter-form__heading" class="block-newsletter-form__heading"><?php _e($heading, 'portent_enercare'); ?></h2>
-			<p data-interface="block-newsletter-form__content" class="block-newsletter-form__content"><?php _e($content, 'portent_enercare'); ?></p>
+			<img data-interface="block-newsletter-form__image" class="block-newsletter-form__image" src="<?= get_template_directory_uri(); ?>/assets/img/join-email-list-24px-thin-r.svg" />
+			<div class="block-newsletter-form__content">
+				<p data-interface="block-newsletter-form__heading" class="block-newsletter-form__heading"><?php _e($heading, 'portent_enercare'); ?></p>
+				<div data-interface="block-newsletter-form__content" class="block-newsletter-form__content"><?php _e($content, 'portent_enercare'); ?></div>
+			</div>
 		</div>
 		<div class="block-newsletter-form__form-container">
 			<?php gravity_form( $gform, false, false ); ?>
