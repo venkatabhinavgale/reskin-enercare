804939a09c242c27244a66b2f7aecd821609bc24
venkatabhinavgale <>
2024-02-20 14:03:38 +0530

    Fix:[ENC-26] | Modified code for campaign variations and component and post level

--- wp-content/themes/portent_enercare_2021/partials/campaign-masthead.php
+++ wp-content/themes/portent_enercare_2021/partials/campaign-masthead.php
@@ -12,7 +12,8 @@
       <p class="block-offer-card__excerpt"><?= $campaign_excerpt ?></p>
     <?php endif; ?>
 
-	  <?php if ($campaign_expiration && $campaign_expiration != "") { ?><p class="block-offer-card__offer-expiration">Offer expires <?= date('F d, Y', strtotime($campaign_expiration)); ?>.<?php } ?><button class="block-offer-card__terms-toggle" aria-controls="terms_<?= $campaign->ID ?>" data-micromodal-trigger="modal-<?=$campaign->ID ?>" aria-label="Open modal and view Terms and Conditions for <?= $campaign_heading ?>">View Terms and Conditions</button></p>
+    <?php $show_hide_expiration_date = get_field('hide_expiry_date', $campaign->ID); ?>
+	  <?php if ($campaign_expiration && $campaign_expiration != "" && !$show_hide_expiration_date) { ?><p class="block-offer-card__offer-expiration">Offer expires <?= date('F d, Y', strtotime($campaign_expiration)); ?>.<?php } ?><button class="block-offer-card__terms-toggle" aria-controls="terms_<?= $campaign->ID ?>" data-micromodal-trigger="modal-<?=$campaign->ID ?>" aria-label="Open modal and view Terms and Conditions for <?= $campaign_heading ?>">View Terms and Conditions</button></p>
 	  <a class="wp-block-button__link has-red-background-color has-background block-offer-card__link" href="<?= $campaign_destination ?>" aria-label="Get deal for <?= $campaign_heading; ?>"><img class="block-offer-card__link__icon" alt="" width="20" height="20" role="presentation" src="<?= $cta_icon ?>"/><span><?= $cta_text ?></span></a>
 	  <?= $campaign_terms; ?>
 </div>
